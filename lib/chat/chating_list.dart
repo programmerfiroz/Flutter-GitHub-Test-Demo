0917013bb8ce5853af37bc74ce22bdec3419d5d0
Firoz Mohammad <78188132+programmerfiroz@users.noreply.github.com>
2024-10-11 20:07:39 +0530

    chating list create

---
+++ lib/chat/chating_list.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ChatingList extends StatefulWidget {
+  const ChatingList({super.key});
+
+  @override
+  State<ChatingList> createState() => _ChatingListState();
+}
+
+class _ChatingListState extends State<ChatingList> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title: const Text("Chating List"),
+      ),
+    );
+  }
+}
